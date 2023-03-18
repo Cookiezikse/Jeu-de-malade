f880fe34558c2f6f7627d377736c342c062509f5
Cookie <yumzuko@gmail.com>
2023-03-19 00:27:46 +0100

    Ajustements + collisions

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 
 func _physics_process(delta):
 
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	move_and_slide()
