93ba856085ec7c6ce03ddd7a13a3b3cef01e3906
Cookie <yumzuko@gmail.com>
2023-03-18 16:45:29 +0100

    Player controls etc...

--- Player.gd
+++ Player.gd
@@ -4,15 +4,16 @@ const SPEED = 300.0
 
 func _physics_process(delta):
 
-
+	var direction2 = Input.get_axis("ui_up", "ui_down")
+	if direction2:
+		velocity.y = direction2 * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-	var direction2 = Input.get_axis("ui_left", "ui_right")
-	if direction2:
-		velocity.y = direction * SPEED
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
