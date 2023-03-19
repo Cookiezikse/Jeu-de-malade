186742b5cff2e8f161516be3afa56ca2d2997463
Cookie <yumzuko@gmail.com>
2023-03-19 12:13:45 +0100

    Changements sur les collision shapes des Tiles

--- Player.gd
+++ Player.gd
@@ -9,10 +9,12 @@ func _physics_process(delta):
 		velocity.y = direction2 * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
+	
 	move_and_slide()
